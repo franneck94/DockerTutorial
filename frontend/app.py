2e70d457c300d89beb1deae9f11a977199632c3a
Jan Schaffranek <schaffranek.jan@web.de>
2025-09-10 17:09:23 +0200

    using uvicorn

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,26 @@
+from fastapi.routing import Mount
 from flask import Flask, render_template
+from starlette.applications import Starlette
+import uvicorn
+from starlette.middleware.wsgi import WSGIMiddleware
 
-app = Flask(__name__)
+flask_app = Flask(__name__)
+app = Starlette(
+    routes=[
+        Mount("/", app=WSGIMiddleware(flask_app)),
+    ],
+)
 
 
-# http://127.0.0.1:5000/
-@app.route("/")
+@flask_app.route("/")
 def index():
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app:app",
+        host="127.0.0.1",  # noqa: S104
+        port=5000,
+        reload=True,
+    )
